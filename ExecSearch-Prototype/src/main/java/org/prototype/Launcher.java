27c3ca2c0fa536629b31c07e233199242d8af959
MiKep-99 <167021379+MiKep-99@users.noreply.github.com>
2024-07-04 18:09:02 +0200

    Add files via upload

---
+++ ExecSearch-Prototype/src/main/java/org/prototype/Launcher.java
@@ -0,0 +1,9 @@
+package org.prototype;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(ExecSearchApplication.class, args);
+    }
+}
